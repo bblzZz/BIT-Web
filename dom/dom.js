cca508ea06848f9b0aac79b1a1b999541fa3fdb8
Gradimir <gradimirbabic2@gmail.com>
2022-12-17 21:59:17 +0100

    dom exercises

---
+++ dom/dom.js
@@ -0,0 +1,71 @@
+
+function selectSec(){
+    var sec = document.querySelector('.second');
+    sec.style.backgroundColor = 'yellow';
+}
+
+ 
+function selectAll(){
+    var selectAll = document.querySelectorAll('li');
+    for(var i=0;i<selectAll.length;i++){
+        selectAll[i].style.backgroundColor = 'green';
+    }
+}
+
+
+function lastSelect(){
+    var ul = document.querySelector('.third')
+    var last = ul.children[ul.children.length-1].classList.add('background');
+    
+     
+     
+}
+
+
+function divLi(){
+    var lastDiv = document.querySelectorAll('div')[1].getElementsByTagName('li')[0].classList.add('active');
+
+}
+
+//divLi()
+
+
+function selectLi(){
+    var active = document.querySelector('.active');
+    active.classList.remove('active');
+    firstLi = document.querySelector('li').classList.add('active');
+    console.log(firstLi);
+}
+
+//selectLi()
+
+function navigation(){
+    var menu = document.getElementById('menu').getElementsByTagName('li')[0];
+     
+    window.alert(menu.textContent)
+
+
+}
+
+//navigation();
+
+
+
+function access(text){
+    var lastLi = document.querySelectorAll('li');
+    var last = lastLi[lastLi.length-1]
+    last.innerText = text;
+}
+
+//access('dddd')
+
+
+//InnerHTML
+
+function dropdown(){
+    var x = document.getElementById("mySelect");
+    var option = document.createElement("option");
+    option.text = "Kiwi";
+    x.add(option);
+}
+dropdown()
